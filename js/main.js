9cf0302247dbc24f0eecbfcfad826e3a61983c91
Abraham Rubio <a.rubio1224@gmail.com>
2023-02-03 16:19:17 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const navToggler = document.querySelector(".nav__toggler");
+const navigation = document.querySelector(".nav");
+
+navToggler.addEventListener("click", function() {
+    navigation.classList.toggle("nav--expanded");
+});
+
+window.onscroll = () => {
+    this.scrollY > 20 ? navigation.classList.add("sticky") : navigation.classList.remove("sticky");
+}
